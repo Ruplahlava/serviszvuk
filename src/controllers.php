b57b419ccd662773caf110176e62741c83facd3e
Stepan Postranecky <stepan.postranecky@gmail.com>
2016-11-19 12:27:24 +0100

    Added basic temlates and content

--- src/controllers.php
+++ src/controllers.php
@@ -6,13 +6,31 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-//Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig', array());
-})
-->bind('homepage')
-;
+});
+
+$app->get('/servis', function () use ($app) {
+    return $app['twig']->render('servis.html.twig', array());
+});
+
+$app->get('/ozvuceni', function () use ($app) {
+    return $app['twig']->render('ozvuceni.html.twig', array());
+});
+
+$app->get('/zakazkova-vyroba', function () use ($app) {
+    return $app['twig']->render('zakazkova-vyroba.html.twig', array());
+});
+
+$app->get('/reference', function () use ($app) {
+    return $app['twig']->render('reference.html.twig', array());
+});
+
+$app->get('/kontakt', function () use ($app) {
+    return $app['twig']->render('kontakt.html.twig', array());
+});
+
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
@@ -21,9 +39,9 @@ $app->error(function (\Exception $e, Request $request, $code) use ($app) {
 
     // 404.html, or 40x.html, or 4xx.html, or error.html
     $templates = array(
-        'errors/'.$code.'.html.twig',
-        'errors/'.substr($code, 0, 2).'x.html.twig',
-        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/' . $code . '.html.twig',
+        'errors/' . substr($code, 0, 2) . 'x.html.twig',
+        'errors/' . substr($code, 0, 1) . 'xx.html.twig',
         'errors/default.html.twig',
     );
 
